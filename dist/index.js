4ed4b9e8e685c800a642e84d62b56cd5d988e3ac
raselinfo <raselinfo52@gmail.com>
2022-04-01 00:28:48 +0600

    Complete Two Number Challe'nge

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,12 @@
 "use strict";
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function myFunction(a, b) {
+    let result = a + b;
+    return result;
+}
+// Test Case
+console.log(myFunction(1, 2));
+console.log(myFunction(1, 10));
+console.log(myFunction(99, 1));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hellow World")
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function myFunction(a: number, b: number): number {
+  let result = a + b;
+  return result;
+}
+
+// Test Case
+console.log(myFunction(1, 2));
+console.log(myFunction(1, 10));
+console.log(myFunction(99, 1));
