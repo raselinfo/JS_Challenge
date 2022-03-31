0a7bf38415ac70563de1f87aceb2299060980a6f
raselinfo <raselinfo52@gmail.com>
2022-04-01 00:47:02 +0600

    Check if the proprty exists in an object

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,17 @@
 "use strict";
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
 function myFunction(a, b) {
-    let result = a + b;
+    let result = a.hasOwnProperty(b);
     return result;
 }
+// Another Solution
+function myFunction2(a, b) {
+    let result = b in a;
+    return true;
+}
 // Test Case
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+console.log(myFunction({ a: 1, b: 2, c: 3 }, "b"));
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "a"));
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "z"));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
 
-function myFunction(a: number, b: number): number {
-  let result = a + b;
+function myFunction(a: object, b: string): boolean {
+  let result = a.hasOwnProperty(b);
   return result;
 }
 
+// Another Solution
+function myFunction2(a:object,b:string):boolean{
+    let result = b in a
+    return true
+}
+
 // Test Case
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+console.log(myFunction({ a: 1, b: 2, c: 3 }, "b"));
+
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "a"));
+
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "z"))
